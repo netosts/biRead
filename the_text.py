9622b22cea6ec969b58d0d82f0dee6365e0255c6
snufera <netostt91@gmail.com>
2023-03-20 19:30:50 -0300

    2.0 Working program

---
+++ the_text.py
@@ -0,0 +1,119 @@
+
+text = """O nascimento de Isaque
+1O Senhor visitou Sara, como tinha dito, e cumpriu o que lhe havia prometido. 2Sara ficou grávida e deu à luz um filho a Abraão na sua velhice, no tempo determinado, de que Deus lhe havia falado. 3Ao filho que lhe nasceu, que Sara lhe dera à luz, Abraão deu o nome de Isaque. 4Abraão circuncidou o seu filho Isaque, quando ele tinha oito dias, segundo Deus lhe havia ordenado. 5Abraão tinha cem anos quando lhe nasceu Isaque, seu filho. 6E Sara disse:
+— Deus me deu motivo de riso. E todo aquele que ouvir isso vai rir comigo.
+7E acrescentou:
+— Quem diria a Abraão que Sara ainda amamentaria um filho? Pois na sua velhice lhe dei um filho.
+8Isaque cresceu e foi desmamado. Nesse dia em que o menino foi desmamado, Abraão deu um grande banquete.
+Agar no deserto
+9Sara viu que o filho que Agar, a egípcia, teve com Abraão estava zombando de Isaque. 10Então Sara disse a Abraão:
+— Mande embora essa escrava e o filho dela, porque o filho dessa escrava não será herdeiro com o meu filho Isaque.
+11Abraão ficou muito incomodado com isso, por causa de seu filho. 12Mas Deus disse a Abraão:
+— Não fique incomodado por causa do menino e por causa da escrava. Faça tudo o que Sara disser, porque por meio de Isaque será chamada a sua descendência. 13Mas também do filho da escrava farei uma grande nação, porque ele é seu descendente.
+14Na manhã seguinte, Abraão levantou-se de madrugada, pegou pão e um odre de água, pôs tudo sobre as costas de Agar, deu-lhe o menino e a despediu. Ela saiu, andando sem rumo pelo deserto de Berseba.
+15Quando acabou a água que havia no odre, Agar colocou o menino debaixo de um dos arbustos. 16E, afastando-se, foi sentar-se em frente, à distância de um tiro de arco, porque dizia:
+— Assim, não verei o menino morrer.
+E, sentando-se em frente dele, levantou a voz e chorou. 17Deus, porém, ouviu a voz do menino. E, do céu, o Anjo de Deus chamou Agar e lhe disse:
+— O que é que você tem, Agar? Não tenha medo, porque Deus ouviu a voz do menino, aí onde ele está. 18Ponha-se em pé, levante o menino e segure-o pela mão, porque eu farei dele um grande povo.
+19Então Deus lhe abriu os olhos, e ela viu um poço de água. E, indo até o poço, encheu o odre de água, e deu de beber ao menino.
+20Deus estava com o menino, que cresceu, morou no deserto e se tornou flecheiro. 21Ele morava no deserto de Parã, e a sua mãe o casou com uma mulher da terra do Egito.
+Abraão faz aliança com Abimeleque
+22Por esse tempo, Abimeleque e Ficol, comandante do seu exército, disseram a Abraão:
+— Deus está com você em tudo o que você faz. 23Portanto, aqui neste lugar, jure por Deus que você não enganará a mim, nem a meu filho, nem a meu neto, e que tratará a mim e a terra em que você tem morado com a mesma bondade com que eu tratei você.
+24Abraão respondeu:
+— Eu juro.
+25Mas Abraão repreendeu Abimeleque por causa de um poço de água que os servos deste haviam tomado à força. 26Abimeleque disse:
+— Não sei quem fez isso. Além do mais, você nunca me falou nada e eu não tinha ouvido nada a respeito, a não ser hoje.
+27Então Abraão pegou ovelhas e bois e os deu a Abimeleque. E os dois fizeram uma aliança. 28Abraão pôs à parte sete cordeiras do rebanho. 29Abimeleque perguntou a Abraão:
+— Que significam as sete cordeiras que você pôs à parte?
+30Abraão respondeu:
+— Você receberá das minhas mãos as sete cordeiras, para que me sirvam de testemunho de que eu cavei este poço.
+31Por isso aquele lugar foi chamado de Berseba, porque ali os dois fizeram um juramento. 32Assim, fizeram aliança em Berseba. Depois Abimeleque e Ficol, comandante do seu exército, voltaram para as terras dos filisteus. 33Abraão plantou uma tamargueira em Berseba e invocou ali o nome do Senhor, o Deus Eterno. 34E por muito tempo Abraão morou na terra dos filisteus."""
+
+text2 = """
+Roupas de Pele: “O Senhor Deus fez roupas de pele e com elas vestiu Adão e sua mulher” (Gênesis 3:21) A Graça começou no próprio Éden – ali começou a Misericórdia de Deus por nós.
+
+E com o ato de Cristo morrer por nós a Graça de Deus nos alcançou. O que é a Graça de Deus?
+
+Em palavras simples GRAÇA significa – “MISERICÓRDIA, FAVOR IMERECIDO”. Ou seja, ninguém merecia a GRAÇA de Deus – mas, por meio do sacrifício de Jesus, Seu Único Filho, essa GRAÇA se estendeu a toda humanidade.
+
+Graça é Deus nos abençoando apesar de não merecermos!
+
+Graça é Deus escolhendo nos abençoar ao invés de amaldiçoar como o nosso pecado merece. É a Sua benevolência a quem não merece.
+
+Graça é o que Deus concede aos homens e que os torna capazes de alcançar a salvação!
+
+“Pois vocês são salvos pela graça, por meio da fé, e isto não vem de vocês, é dom de Deus” (Efésios 2:8)
+
+Um grande movimento do perdão de Deus vemos na atitude Dele para com Adão e Eva quando Ele lhes prepara roupas de pele. Neste ato de Deus notamos que um animal morreu no lugar deles já simbolizando o sacrifício de Cristo.
+
+A morte de um animal já anunciava a morte de Cristo em razão dos nossos pecados. A atitude de Deus com a morte do animal tipificam e simboliza o perdão do pecado de Adão e Eva pelo sangue do animal. Assim como a morte de Cristo para o perdão do mundo.
+
+A Está altura você pergunta se não existe morte no Paraíso, como um animal foi morto por Deus? – A primeira compreensão aqui é que Deus é o dono da vida, Ele decide – Ele cria, Ele decide! E a Segunda é que na Bíblia não diz “o local” onde o animal foi morto, assim como também não relata a morte de um animal específico.
+
+Na Bíblia diz “apenas” que Deus fez “roupas de pele” o que nos leva ao entendimento claro de um sacrifício rico em significados.
+
+O animal sacrificado para cobrir a vergonha do pecado, representa Cristo que seria a nossa redenção. Nós temos a maldição do pecado na nossa carne por meio de Adão, pois somos sua descendência. Por meio de Cristo temos a redenção dessa maldição. (Redenção significa: Resgate do gênero humano por Jesus Cristo é uma proteção que livra de situação difícil; salvação)
+
+Por meio de Jesus Cristo, Deus nos perdoa e nos dá uma nova Vida. O que Deus fez então? Transferiu a culpa para Jesus Cristo e este pagou para garantir de volta o restabelecimento de Adão e seus descendentes.
+
+“Pois vocês sabem que não foi por meio de coisas perecíveis como prata ou ouro que vocês foram redimidos da sua maneira vazia de viver que lhes foi transmitida por seus antepassados, mas pelo precioso sangue de Cristo, como de um cordeiro sem mancha e sem defeito” (1 Pedro 1:18,19)
+
+Jesus é a nossa válvula de escape! A cruz alcança o passado, o presente e o futuro.
+
+É igualmente importante ver que a primeira morte, tanto dos animais quanto dos homens, não foi “natural”, mas violenta. O sangue deles foi derramado. O derramamento de sangue é um fato temido da vida em todo lugar. Todos os animais não predadores ficam muito excitados e descontrolados com o cheiro de sangue, especialmente de sua própria espécie. Os homens são naturalmente adversos à idéia de derramamento de seu sangue e a Bíblia é bastante eloqüente sobre o mal do derramamento de sangue.
+
+Apesar de tudo isso, aprendemos que foi Deus quem primeiro derramou sangue na cofecção de roupas para cobrir a nudez de Adão e Eva. É óbvio que Deus providenciou essas peles matando animais, e o padrão de sacrifício animal do Velho Testamento certamente poderá sustentar essa conclusão. Assim, a questão que nos encara é: “Por que Deus matou os animais? Por que o derramamento de sangue?”
+
+Quando Adão pecou, não apenas ele, mas sua esposa, toda a sua descendência e a criação inteira (a terra e todos os seus habitantes) foram afastados de Deus. Essa rebelião também afetou os animais, não no pecado, o que pode ser facilmente visto pelo fato de que os animais, que deveriam comer apenas ervas e agora comem uns aos outros.
+
+Adão não estava em comunhão com Deus. Estava com medo. Sentiu a sua nudez e a ira iminente de Deus sobre ele. Seguindo isso, em (Gênesis 3), vemos o pronunciamento do julgamento de Deus e de todas as penalidades do pecado. Mas, no meio desse julgamento, vemos um ato de Deus (fez roupas de pele) para prover uma necessidade do homem (e eu estava com medo, por que eu estava nu; e eu me escondi).
+
+“Ouvindo o homem e sua mulher os passos do Senhor Deus que andava pelo jardim quando soprava a brisa do dia, esconderam-se da presença do Senhor Deus entre as árvores do jardim. Mas o Senhor Deus chamou o homem, perguntando: “Onde está você? “E ele respondeu: “Ouvi teus passos no jardim e fiquei com medo, porque estava nu; por isso me escondi” (Gênesis 3:8,10)
+
+Vemos mais uma vez neste trecho Deus e a Sua Graça/Misericórdia em favor de Adão e Eva e sua descendência (Eu e Você)
+
+De acordo com (Hebreus 10) e muitas outras Escrituras, esse primeiro derramamento de sangue teve um significado mais profundo e mais maravilhoso. Vemos que os sacrifícios de sangue tiveram uma relação com a purificação do pecado.
+
+“A Lei traz apenas uma sombra dos benefícios que hão de vir, e não a realidade dos mesmos. Por isso ela nunca consegue, mediante os mesmos sacrifícios repetidos ano após ano, aperfeiçoar os que se aproximam para adorar. Se pudesse fazê-lo, não deixariam de ser oferecidos? Pois os adoradores, tendo sido purificados uma vez por todas, não mais se sentiriam culpados de seus pecados. Contudo, esses sacrifícios são uma recordação anual dos pecados, pois é impossível que o sangue de touros e bodes tire pecados. Por isso, quando Cristo veio ao mundo, disse: “Sacrifício e oferta não quiseste, mas um corpo me preparaste; de holocaustos e ofertas pelo pecado não te agradaste”. Então eu disse: Aqui estou, no livro está escrito a meu respeito; vim para fazer a tua vontade, ó Deus. Primeiro ele disse: “Sacrifícios, ofertas, holocaustos e ofertas pelo pecado não quiseste, nem deles te agradaste” ( os quais eram feitos conforme a Lei ). Então acrescentou: “Aqui estou; vim para fazer a tua vontade”. Ele cancela o primeiro para estabelecer o segundo. Pelo cumprimento dessa vontade fomos santificados, por meio do sacrifício do corpo de Jesus Cristo, oferecido uma vez por todas. Dia após dia, todo sacerdote apresenta-se e exerce os seus deveres religiosos; repetidamente oferece os mesmos sacrifícios, que nunca podem remover os pecados. Mas quando este sacerdote acabou de oferecer, para sempre, um único sacrifício pelos pecados, assentou-se à direita de Deus. Daí em diante, ele está esperando até que os seus inimigos sejam colocados como estrado dos seus pés; porque, por meio de um único sacrifício, ele aperfeiçoou para sempre os que estão sendo santificados. O Espírito Santo também nos testifica a este respeito. Primeiro ele diz: “Esta é a aliança que farei com eles, depois daqueles dias, diz o Senhor. Porei as minhas leis em seus corações e as escreverei em suas mentes”; e acrescenta: “Dos seus pecados e iniqüidades não me lembrarei mais”. Onde essas coisas foram perdoadas, não há mais necessidade de sacrifício pelo pecado. Portanto, irmãos, temos plena confiança para entrar no Santo dos Santos pelo sangue de Jesus, por um novo e vivo caminho que ele nos abriu por meio do véu, isto é, do seu corpo. Temos, pois, um grande sacerdote sobre a casa de Deus. Sendo assim, aproximemo-nos de Deus com um coração sincero e com plena convicção de fé, tendo os corações aspergidos para nos purificar de uma consciência culpada e tendo os nossos corpos lavados com água pura” (Hebreus 10:1,22)
+
+Aprendemos que o sangue de touros e bodes não podem tirar os pecados. Então, aprendemos que o sangue de Cristo de fato tira o pecado. Agora, com essas duas verdades opostas em mente, você percebe que a proposta de Deus ao sacrificar os animais (derramando seu sangue) e todo sacrifício do Velho Testamento. Foi para testemunhar (em símbolo) o sacrifício reconciliador de Cristo.
+
+O sangue na Bíblia: Sangue representa a Vida!
+
+O sangue humano da á vida física ao que necessita. O sangue é um presente de Deus, porque Deus é vida e Dele depende nossa existência. Deus é o autor de toda a vida, Ele fez de um sangue todas as nações. Sangue é algo precioso aos olhos de Deus que ama toda a sua criação.
+
+O sangue deve ser tratado como algo especial, ele é tão especial que salva vidas. O próprio Jesus Cristo deu o “Seu Sangue” para salvar todo pecador.
+
+Jesus derramou o seu sangue por nós, ou seja, fez uma transfusão de vida eterna para todo aquele que Nele crê. Ele fez a transfusão de sangue Dele para toda a humanidade para nos purificar de todo pecado.
+
+Todo o plano de redenção (salvação) do homem gira em torno do sangue. O sangue é o único meio de compra da redenção.
+
+Jesus suou sangue, Ele entrou em agonia no Jardim Getsêmani e seu suor tornou-se como gotas de sangue a escorrer pela terra, na cruz o seu lado foi perfurado saindo sangue e água, “Contudo, um dos soldados furou seu lado com uma lança, e daí correu sangue com água.” (João: 19,34)
+
+O suar sangue, ou “HEMATIDROSE” é um fenômeno raríssimo e produzido em condições excepcionais. Para provocá-lo é necessário um fraqueza física, acompanhada de um abatimento moral violento causado por uma profunda emoção.
+
+O terror, o susto, a angústia terrível de sentir-se carregando todos os pecados do mundo devem ter literalmente esmagado Jesus. Tal tensão extrema produz o rompimento das finíssimas veias capilares que estão sob as glândulas sudoríparas (são glândulas que produzem o suor, função importante para regular a temperatura do corpo e eliminar substâncias tóxicas)
+
+É o sangue que dá continuidade a todos os processos da vida, no corpo. Se o suprimento de sangue for cortado de um braço, este imediatamente começará a morrer e apodrecer. Através do sangue de Jesus voltamos a ter vida e aquele que está conectado ao “Corpo Cristo” não morrerá mais (Me Refiro ao Batismo nas Águas e a Salvação Eterna)
+
+(Para Entender a Respeito do Batismo – CLICA AQUI)
+
+Para Emtender a Respeito da Salvação Eterna – CLICA AQUI)
+
+FOMOS COMPRADOS A PREÇO DE SANGUE. A VIDA ESTÁ NO SANGUE!
+
+“Vocês foram comprados por alto preço. Portanto, glorifiquem a Deus com o corpo de vocês” (1 Coríntios 6:20)
+
+Para nos ensinar sobre o perdão e o seu alcance, Deus mostrou no Éden, que o perdão é para “quem não merece”.
+
+Vemos que Deus não lhes atirou as vestes para que se vestissem, mas vestiu-os, com todo o Seu amor e carinho. Só o Amor desperta o amor”.
+
+Amor não é sentimento e sim um ato! E nas atitudes/ações, no comportamento que o vemos o amor não está em Palavras vazias, ele mora nos detalhes. Pense em como aquele Amor foi Transcendente (Transcendente significa que excede os limites normais; superior, sublime. Que transcende a natureza física das coisas; metafísico). Dali em diante, conheceram profundamente o Amor de Deus. O perdão frente à oferta imerecida.
+
+Que o Espírito Santo gere em nós o caráter de Cristo para que frente às consequências, assumamos firmemente as decisões que tomamos, proveniente das escolhas que fazemos. Errando mas pedindo perdão e assumindo suas consequências. Somente assim, poderemos gerar em nossos corações o arrependimento que Deus aguardava no Éden, e então, provaremos do perdão que nos reconcilia com o Eterno por meio da cruz de Cristo.
+
+O verdadeiro cristão tem a bíblia como um manual para sua existência, ela é a nossa regra de FÉ e PRÁTICA. Tudo aquilo que precisamos saber e toda orientação que possamos querer estão contidas neste livro.
+
+Deus abençoe á todos!
+"""
