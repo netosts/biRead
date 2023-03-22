698b8438dac890219b111beb3618373345d03170
snufera <netostt91@gmail.com>
2023-03-22 19:55:34 -0300

    list of words

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import inch
 from reportlab.platypus import Paragraph, Frame
-import re
-from text import text
+from the_text import text2
 
 
 # This is the text that will be formatted
 # to change the text, go to text.py and change it there
-text = text
+text = text2
 
 # Create a new PDF file
 ask = str(input("Nome do arquivo: ")).strip()
@@ -17,57 +16,65 @@ pdf_file = f"{ask}.pdf"
 c = canvas.Canvas('C:/Users/netos/OneDrive/Documentos/Readable PDF files/'+pdf_file, pagesize=letter)
 
 # Define the paragraph style for the text
-normal_style = ParagraphStyle(name='Normal', fontName='Helvetica', fontSize=11, leading=22)
+normal_style = ParagraphStyle(name='Normal', fontName='Helvetica', fontSize=2, leading=22, splitLongWords=False)
 
 # Set the margins and dimensions of the frame to fit the page
-x, y = 1*inch, 0.5*inch
+x, y = 1*inch, 0.8*inch
 width, height = letter[0]-2.2*inch, letter[1]-1.5*inch
 frame = Frame(x, y, width, height, showBoundary=0)
 
-# Create a new paragraph object and add it to the frame
-lines = text.splitlines()
-indent = '&nbsp;&nbsp;&nbsp;'
-is_first_line = True
 
-formatted_text = ''
-for line in lines:
-    if line.strip():
-        if is_first_line:
-            formatted_text += indent*4
-            if_first_line = False
+# Define a function to create a formatted text with a list of words
+def bionic_text(text):
+    # Split text in paragraphs
+    lines = text.splitlines()
+    indent = '&nbsp;&nbsp;&nbsp;'
 
-        words = re.findall(r'\S+|\s+', line)
-        for l in range(len(words)):
-            if words[l].isspace():
-                formatted_text += words[l]
-            else:
-                match = re.match(r'^(\d+)(\D.*)', words[l])
-                if match:
-                    # word starts with a number, split it into two parts
-                    number = match.group(1)
-                    word = match.group(2)
-                    if len(word) == 1:
-                        index_to_bold = 1
-                    else:
-                        index_to_bold = len(word) // 2
-                    formatted_word = f"{number}<b>{word[:index_to_bold]}</b>{word[index_to_bold:]}"
-                else:
-                    # word doesn't start with a number
-                    index_to_bold = len(words[l]) // 2
-                    formatted_word = ''
-                    for i, letter in enumerate(words[l]):
+    formatted_text = list()
+    for line in lines:
+        if line.strip():
+            # split paragraphs in words to format it
+            words = line.split()
+            for l in range(len(words)):
+                # Bionic bold each word
+                index_to_bold = len(words[l]) // 2
+                formatted_word = ''
+                for i, letter in enumerate(words[l]):
+                    if letter.isalpha():  # check if letter is a word character
                         if i < index_to_bold:
                             formatted_word += '<b>' + letter + '</b>'
                         elif len(words[l]) == 1:
                             formatted_word += '<b>' + letter + '</b>'
+                        elif words[l][0] in '“' and i == 1:
+                            formatted_word += '<b>' + letter + '</b>'
                         else:
                             formatted_word += letter
-                formatted_text += formatted_word
-        formatted_text += '<br/>'
-        is_first_line = True
+                    else:
+                        formatted_word += letter
+                if l == 0:  # indent the first line of each paragraph
+                    formatted_text.append(indent*4 + formatted_word + ' ')
+                elif l == (len(words) - 1):  # break line at the end of paragraph
+                    formatted_text.append(formatted_word + '<br/>')
+                else:
+                    formatted_text.append(formatted_word + ' ')
+    return formatted_text  # return the list of words
 
-paragraph = Paragraph(formatted_text, normal_style)
-frame.addFromList([paragraph], c)
+
+# Iterate through the words and add them to the canvas
+words = bionic_text(text).split()
+for w in words:
+    # Create a new paragraph object and add it to the frame
+    paragraph = Paragraph(w, normal_style)
+
+    # Check if the paragraph fits in the current frame
+    if frame.add(paragraph, c):
+        # The paragraph fits, so continue to the next paragraph
+        continue
+    else:
+        # The paragraph doesn't fit, so create a new page and add the paragraph to it
+        c.showPage()
+        frame = Frame(x, y, width, height, showBoundary=0)
+        frame.add(paragraph, c)
 
 # Save the PDF file and close the canvas
 c.save()
